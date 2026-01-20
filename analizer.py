3eab003edac66af7426f28b8f384fb3033293b6b
grassonglass <grassgeneral@163.com>
2026-01-20 20:23:46 +0800

    More specific model.

--- analizer.py
+++ analizer.py
@@ -10,7 +10,7 @@ encode_sequence = json.load(f)
 name_sequence = encode_sequence["name"]
 f.close()
 class analizer:
-    def __init__(self, data: bytes,file:str):
+    def __init__(self, file="./test.mca", data: bytes=None):
         if data is None:
             with open(file, 'rb') as f:
                 data = f.read()
@@ -18,7 +18,7 @@ class analizer:
         else:
             self.data = data
 
-    def getChunkData(self,x,z)->bytes:
+    def chunkData(self,x,z)->bytes:
         off = 4 * (x % 32 + z % 32 * 32)
         off = int.from_bytes(self.data[off : off + 3],byteorder='big')
         if off == 0 and self.data[off + 3] == 0:
@@ -30,16 +30,18 @@ class analizer:
         return zlib.decompress(self.data[off + 5 : off + 5 + length - 1])
 
     @staticmethod
-    def findAll(data:bytes,string):
+    def findAll(string, data:bytes=None)->list:
         '''
         search the data with given string
         desinged to avoid analize all the tags
         return : list of indexs 
         '''
+        if data is None:
+            raise ValueError("data can't be None")
         _range = len(data) - len(string)
         return [i for i in range(_range) if string.encode() == data[i:i+len(string)]]
     
-    def paletteMap(self,data:bytes,encode_sequence=encode_sequence,name_sequence=name_sequence):
+    def paletteMap(self,data:bytes)->np.ndarray:
         '''
         this method will return the compund list of encoded properties 
         data: the palette of one setion, starts from block_states to biome, nbt tag or json bytes?
@@ -49,7 +51,7 @@ class analizer:
         if data is None:
             return [0 for i in range(10)]#air, the section is empty
         
-        indexs = self.findall('Name')
+        indexs = self.findAll('Name')
         code_list = [[] for i in range(len(indexs))]
         #encode logic has to be written by hand, which is really a frustrating thing.
         for i in range(len(indexs)): 
@@ -94,17 +96,97 @@ class analizer:
             properties = data[indexs[i]:indexs[i+1]]
             #to make sure states won't affect each other
             #in the first loop, find the indexs of all the states
+            #as the aobve code has excluded many illegal states, there won't be error
             state_indexs = []
             for j in encode_sequence["state"]:
                 state_indexs.append(self.findall(j,properties)[0])
-            #then, encode the states
+            #then, encode the states 
             for j in state_indexs:
                 for k in encode_sequence["state"][j]:
                     if k in properties[j:state_indexs[j+1]]:
-                        code_list[i].append(encode_sequence["state"][j][k])
+                        code_list[i].extend(encode_sequence["state"][j][k])
         code_list = np.array(code_list)
         return code_list
-
+    
+    def chunkMap(self,x,z)->np.ndarray:
+        data = self.chunkData(x,z)
+        if data is None:
+            return None
+        chunk = np.zeros((32,4096,12),dtype=np.float32)
+        section_indexs = self.findAll('BlockStates',data)
+        for i in range(len(section_indexs)):
+            section = data[section_indexs[i]:section_indexs[i+1]]
+            chunk[i] = self.paletteMap(section)
+        return chunk
+    
+    def regionMap(self,region_x,region_z):
+        '''
+        return the whole region data as a numpy array
+        shape: (1024,4096,12)
+        '''
+        region = np.zeros((1024,4096,12),dtype=np.float32)
+        for i in range(32):
+            for j in range(32):
+                chunk = self.chunkMap(region_x*32 + i, region_z*32 + j)
+                if chunk is not None:
+                    yield region
+    
+    def saveRegion(self, region_x, region_z, path):
+        '''
+        give the coordinate or region file path to save the region data as a numpy file
+        1024 chunks in a file, saved with np.savez so that it can be loaded easily
+        '''
+        region_data = self.regionMap(region_x, region_z)
+        np.savez(path, region_data)
+    
     @staticmethod
-    def blockMapFromGame(self,data:bytes):
-        pass
+    def blockMapfromJson(self,data:bytes):
+        '''
+        json file should be a sequence of blocks(4096)
+        each block should be a dict including 'name' and 'states'
+        '''
+        json_data = json.loads(data.decode())
+        code_list = []
+        for block in json_data:
+            name = block['name']
+            code_block = []
+            #model
+            for j in encode_sequence["model"]:
+                if j in name:
+                    code_block.append(encode_sequence["model"][j])
+            #texture
+            if name in name_sequence["nature"]:
+                code_block.append(name_sequence["nature"][name])
+            elif name in name_sequence["metal"]:
+                code_block.append(name_sequence["metal"])
+            else:
+                def encode(s):
+                    for j in name_sequence[s][0]:
+                        if j in name:
+                            for k in name_sequence[s][1]:
+                                if k in name:
+                                    return name_sequence[s][0][j].append(
+                                           name_sequence[s][1][k])
+                _ = encode("coloured_block")
+                if _ != None:
+                    code_block = _
+                else:
+                    _ = encode("wood")
+                    if _ != None:
+                        code_block = _
+                    else:
+                        _ = encode("stone")
+                        if _ != None:
+                            code_block = _
+                        else:
+                            code_block == []#the block isn't in the encoding_sequence
+                            code_block = [0 for i in range(10)]#fill 'air' instead
+            #state
+            properties = block['states']
+            for j in properties:
+                if j in encode_sequence["state"]:
+                    if properties[j] in encode_sequence["state"][j]:
+                        code_block.extend(encode_sequence["state"][j][properties[j]])
+            code_list.append(code_block)
+        code_list = np.array(code_list)
+        return code_list
--- model.py
+++ model.py
@@ -3,55 +3,43 @@ from torch import nn
 import numpy as np
 from torch.nn import functional as F
 
+LEARN_RATE = 0.0005
+BATCH_SIZE = 1#just for test, real batch size should be larger
 
+class EndofBlock(Exception):
+    pass
 
 class Embedding(nn.Module):
-    def __init__(self, vocab_size, embedding_dim):
+    def __init__(self):
         '''
         Trained before the model, and will be load when the model is trained.
+        basic encodding has been done in analizer.py, this embedding layer is used to
+        enlarge the encoding dimension, making it sparse and having more complex linear features
         '''
         super(Embedding, self).__init__()
-        self.embedding = nn.Linear(vocab_size, embedding_dim)
+        self.embedding = nn.Linear(12, 128)
+        self.optimizer = torch.optim.Adagrad(self.parameters(), lr=LEARN_RATE*2)
 
     def forward(self, x):
+        '''
+        return a new tensor, in which each row is the embedding of the input row
+        x:(4096, 12)
+        return:(4096, 128)
+        '''
         return self.embedding(x)
     
-class output(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(output, self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
+class Output(nn.Module):
+    def __init__(self):
+        super(Output, self).__init__()
+        self.linear = nn.Linear(128, 12)
 
     def forward(self, x):
         return self.linear(x)
     
-class Model(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim):
-        '''
-        Transformer is the core of our model.
-        But due to performance and memory limitation, 
-        parameters can't be enormous. Thus it may need extra help.
-        '''
-        super(Model, self).__init__()
-        self.embedding = Embedding(vocab_size, embedding_dim)
-
-        self.transformer = nn.Sequential( 
-            nn.TransformerDecoderLayer(embedding_dim, hidden_dim, batch_first=True),
-            nn.TransformerDecoderLayer(embedding_dim, hidden_dim, batch_first=True),
-            nn.TransformerDecoderLayer(embedding_dim, hidden_dim, batch_first=True)
-        )
-        self.output = output(hidden_dim, output_dim)
-    
-    def forward(self, x):
-        x = self.embedding(x)
-        x = self.transformer(x)
-        x = self.output(x)
-        return x
-    
-    def load_embeddings(self, embeddings):
-        self.embedding.embedding.weight.data.copy_(torch.from_numpy(embeddings))
 
 class Decider(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self):
+        #input:(4096, 12)
         '''
         when training, there must be lots of 'natural' data, which should not 
         be used to train the model.
@@ -63,7 +51,114 @@ class Decider(nn.Module):
         reason the structure if there's no useful prompts.
         '''
         super(Decider, self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
+        self.squeeze = nn.Parameter(torch.randn((1,4096),dtype=torch.float32))
+        self.linear = nn.Sequential(
+            nn.Linear(12, 512),
+            nn.ReLU(),#4096*512
+            nn.BatchNorm1d(512),
+            nn.Linear(512, 128),
+            nn.ReLU(),#4096*128
+            nn.BatchNorm1d(128),
+            nn.Dropout(0.1),
+            nn.Linear(128, 1),
+        )
+        self.optimizer = torch.optim.Adagrad(self.parameters(), lr=LEARN_RATE/2)
 
     def forward(self, x):
-        return self.linear(x)
+        x =  self.linear(x)#4096*1
+        return torch.sigmoid(self.squeeze @ x)#1*1
+
+class DecoderLayer(nn.TransformerDecoderLayer):
+    def __init__(self):
+        super(DecoderLayer, self).__init__(d_model=128, nhead=4, dim_feedforward=512, dropout=0.05, activation='relu')
+    def forward(self, x, me):
+        x = super().forward(x, me, memory_is_causal=True)#use look-ahead mask by default
+        return x
+
+class Model(nn.Module):
+    def __init__(self):
+        #input:(4096, 12)
+        '''
+        Transformer is the core of our model.
+        But due to performance and memory limitation, 
+        parameters can't be enormous. Thus it may need extra help.
+        position has been encoded in the input data, maybe we can add more positional encoding later?
+        '''
+        if torch.cuda.is_available():
+            torch.set_default_tensor_type(torch.cuda.FloatTensor)
+            torch.device("cuda")
+        super(Model, self).__init__()
+        self.decider = Decider()
+        self.embedding = Embedding()
+
+        self.transformerdecodelayer = DecoderLayer(d_model=128, nhead=4, dim_feedforward=512, dropout=0.05, activation='relu')
+        self.transformer = nn.TransformerDecoder(num_layers=8, decoder_layer=self.transformerdecodelayer, norm=nn.LayerNorm(128))
+
+        self.output = Output()
+
+        self.memorystack = torch.zeros((8,4096,12),dtype=torch.float32) #a simple memory stack
+        self.lossfunc = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=LEARN_RATE)
+    
+    def forward(self, src):
+        '''
+        src are both tensor with shape (4096, 12)
+        with every src, the model should predict for 4096 times
+        and update its memory stack with the new prediction
+        the model is to modify the input src to a better version
+        thus mask doesn't matter here
+        return: tensor with shape (1, 12)
+        '''
+        if self.decider(src).item() < 0.2:
+            return EndofBlock #skip the useless data
+        src = self.capWithMemory(src)#(8192,12)
+        src = self.embedding(src)#(8192,128)
+        src = self.transformer((1,src, src))#(1,8192,128)
+        src = self.output(src)#(1,12)
+        return src
+    
+    def load_embeddings(self, embeddings):
+        self.embedding.embedding.weight.data.copy_(torch.from_numpy(embeddings))
+
+    def memoryUpdate(self, new_memory):
+        '''
+        update the memory stack with new memory
+        if the stack is full, pop the oldest memory
+        new_memory: tensor with shape (4096,)
+        '''
+        self.memorystack = torch.roll(self.memorystack, shifts=-1, dims=0)
+        self.memorystack[-1] = new_memory
+
+    def capWithMemory(self, x):
+        '''
+        cap the input with memory stack
+        记忆栈会被以下方式加权平均：
+        由栈内记忆数量计算一次softmax函数,以记忆的下标倒置为输入
+        例如,栈内有4条记忆,则输入为[4,3,2,1],输出为softmax([4,3,2,1])
+        这样可以让较新的记忆拥有更高的权重
+        同时减小计算量
+        //translate, not good at english sorry for that
+        The memory stack will be weighted averaged as follows:
+        Calculate a softmax function based on the number of memories in the stack,
+        with the index of the memory reversed as input.
+        For example, if there are 4 memories in the stack, the input is [4
+        ,3,2,1], and the output is softmax([4,3,2,1]).
+        This allows newer memories to have higher weights
+        while reducing the amount of computation.
+        x: tensor with shape (4096, 12)
+        return: tensor with shape (8192, 12)
+        '''
+        #omit the empty memory
+        memory_size = 0
+        for i in range(self.memorystack.shape[0]):
+            if torch.all(self.memorystack[i] == 0):
+                continue
+            memory_size += 1
+        if memory_size == 0:
+            return torch.cat([x,torch.zeros((4096, 12), dtype=torch.float32)],dim=0)#(8192,12)
+        
+        weights = F.softmax(torch.arange(memory_size,0,-1).float(), dim=0)#(1, memory_size)
+        weights = weights.view(-1,1,1)
+        weighted_memory = torch.sum(self.memorystack[:memory_size] * weights.unsqueeze(2), dim=0)#(4096,12)
+        return torch.cat((weighted_memory, x), dim=0)#(8192,12)
+
